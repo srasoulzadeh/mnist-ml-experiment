350576ad98a2d1950449ae874ce58654c719d101
Shervin Rasoulzadeh <srasoulzadeh@cg.tuwien.ac.at>
2021-01-23 00:32:05 +0100

    tensorflow lite replaced by barracuda

--- Assets/Scripts/MnistSample.cs
+++ Assets/Scripts/MnistSample.cs
@@ -1,42 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Unity.Barracuda;
 using UnityEngine;
 using UnityEngine.UI;
-using TensorFlowLite;
-
 
 public class MnistSample : MonoBehaviour
 {
-    [SerializeField, FilePopup("*.tflite")] string fileName = "mnist.tflite";
+    public NNModel modelAsset;
+    private Model m_RuntimeModel;
+    private IWorker m_Worker;
+
     [SerializeField] Text outputTextView = null;
     [SerializeField] ComputeShader compute = null;
-
-    Interpreter interpreter;
 
     bool isProcessing = false;
     float[,] inputs = new float[28, 28];
     float[] outputs = new float[10];
     ComputeBuffer inputBuffer;
 
+    
     System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+    private 
 
     void Start()
     {
-        var options = new InterpreterOptions()
-        {
-            threads = 2,
-            useNNAPI = false,
-        };
-        interpreter = new Interpreter(FileUtil.LoadFile(fileName), options);
-        interpreter.ResizeInputTensor(0, new int[] { 1, 28, 28, 1 });
-        interpreter.AllocateTensors();
+        this.m_RuntimeModel = ModelLoader.Load(modelAsset);
+        this.m_Worker = WorkerFactory.CreateWorker(m_RuntimeModel);
 
         inputBuffer = new ComputeBuffer(28 * 28, sizeof(float));
     }
 
     void OnDestroy()
     {
-        interpreter?.Dispose();
         inputBuffer?.Dispose();
     }
 
@@ -44,34 +38,23 @@ public class MnistSample : MonoBehaviour
     {
         if (!isProcessing)
         {
-            Invoke(texture);
+            isProcessing = true;
+
+            compute.SetTexture(0, "InputTexture", texture);
+            compute.SetBuffer(0, "OutputTensor", inputBuffer);
+            compute.Dispatch(0, 28 / 4, 28 / 4, 1);
+            inputBuffer.GetData(inputs);
+
+            var input = new Tensor(texture, 1);
+            var output = this.m_Worker.Execute(input).PeekOutput().Flatten();
+
+            int predicted_label = output.ArgMax()[0];
+
+            sb.Clear();
+            sb.AppendLine($"{predicted_label}");
+            outputTextView.text = sb.ToString();
+
+            isProcessing = false;
         }
-    }
-
-    void Invoke(RenderTexture texture)
-    {
-        isProcessing = true;
-
-        compute.SetTexture(0, "InputTexture", texture);
-        compute.SetBuffer(0, "OutputTensor", inputBuffer);
-        compute.Dispatch(0, 28 / 4, 28 / 4, 1);
-        inputBuffer.GetData(inputs);
-
-        float startTime = Time.realtimeSinceStartup;
-        interpreter.SetInputTensorData(0, inputs);
-        interpreter.Invoke();
-        interpreter.GetOutputTensorData(0, outputs);
-        float duration = Time.realtimeSinceStartup - startTime;
-
-        sb.Clear();
-        sb.AppendLine($"Process time: {duration: 0.00000} sec");
-        sb.AppendLine("---");
-        for (int i = 0; i < outputs.Length; i++)
-        {
-            sb.AppendLine($"{i}: {outputs[i]: 0.00}");
-        }
-        outputTextView.text = sb.ToString();
-
-        isProcessing = false;
     }
 }
